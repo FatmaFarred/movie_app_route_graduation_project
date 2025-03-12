ed12e78ac15467a4bde74e66a038b490e7961ef5
youssefmdev22 <youssefmdev22@gmail.com>
2025-03-12 06:59:32 +0200

    + add assets

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app_route_graduation_project/core/utilis/App_colors.dart';
 
-
-
 class AppTheme{
   static ThemeData themeData = ThemeData(
       scaffoldBackgroundColor: AppColors.blackColor,
