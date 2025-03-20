0ba367245208f0c7d02fe10a009dc63216adbeae
fatma <fatmafarred267@gmail.com>
2025-03-21 02:27:37 +0300

    added the auto login feature added movie details logic

--- lib/data/data_sources/remote_data_sources/Movie_Deatils_Remote_Data_Source_impl.dart
+++ lib/data/data_sources/remote_data_sources/Movie_Deatils_Remote_Data_Source_impl.dart
@@ -1,56 +1,57 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dartz/dartz.dart';
-import 'package:injectable/injectable.dart';
-import 'package:movie_app_route_graduation_project/Api%20manager/Api_constant.dart';
-import 'package:movie_app_route_graduation_project/Api%20manager/Api_manager.dart';
-import 'package:movie_app_route_graduation_project/Api%20manager/end_points.dart';
-import 'package:movie_app_route_graduation_project/Api%20manager/errors/failure.dart';
-import 'package:movie_app_route_graduation_project/data/model/MovieDetailsResponseDm.dart';
-import 'package:movie_app_route_graduation_project/domain/entities/MovieDetailsResponseEntity.dart';
-import 'package:movie_app_route_graduation_project/domain/entities/MovieSuggetionResponseEntity.dart';
-import 'package:movie_app_route_graduation_project/domain/repositries/data_sources/remote_data_sources/Movie%20_Details_data_source.dart';
-@Injectable(as: MovieDetailsDataSource)
-class MovieDetailsRemoteDataSourceImpl implements MovieDetailsDataSource{
+
+import '../../../Api manager/Api_constant.dart';
+import '../../../Api manager/Api_manager.dart';
+import '../../../Api manager/end_points.dart';
+import '../../../Api manager/errors/failure.dart';
+import '../../../domain/entities/MovieDetailsResponseEntity.dart';
+import '../../../domain/entities/MovieSuggetionResponseEntity.dart';
+import '../../../domain/repositries/data_sources/remote_data_sources/Movie _Details_data_source.dart';
+import '../../model/MovieDetailsResponseDm.dart';
+import '../../model/MovieSuggetionResponseDm.dart' show MovieSuggetionResponseDm;
+
+class MovieDetailsRemoteDataSourceImpl implements MovieDetailsDataSource {
+   ApiManager apiManager;
+
+  MovieDetailsRemoteDataSourceImpl({required this.apiManager});
+
   @override
-  ApiManager apiManager ;
-  MovieDetailsRemoteDataSourceImpl ({required this.apiManager})
-  Future<Either<Failure, MovieDetailsResponseEntity>> getMovieDeatials(int id)async {
+  Future<Either<Failure, MovieDetailsResponseDm>> getMovieDetails(int id) async {
     try {
-      final List<ConnectivityResult> connectivityResult = await Connectivity()
-          .checkConnectivity();
+      final List<ConnectivityResult> connectivityResult = await Connectivity().checkConnectivity();
       if (connectivityResult.contains(ConnectivityResult.wifi) ||
           connectivityResult.contains(ConnectivityResult.mobile)) {
         var response = await apiManager.getData(
           endPoint: EndPoints.MovieDetails,
           url: ApiConstant.baseMovieUrl,
-          queryParameters: {"movie_id": id,
+          queryParameters: {
+            "movie_id": id,
             "with_cast": true,
-            "with_images": true
+            "with_images": true,
           },
         );
-        var MoviedetailsResponse = MovieDetailsResponseDm.fromJson(
-            response.data);
+        var movieDetailsResponse = MovieDetailsResponseDm.fromJson(response.data);
+
+
         if (response.statusCode! >= 200 && response.statusCode! < 300) {
-          return right(MoviedetailsResponse);
+          return right(movieDetailsResponse);
         } else {
           return left(
-              serverError(errorMessage: MoviedetailsResponse.statusMessage));
+            serverError(errorMessage: movieDetailsResponse.statusMessage ?? 'Unknown error'),
+          );
         }
       } else {
-        return left(NetworkError(errorMessage: "No Ienternet Connection"));
+        return left(NetworkError(errorMessage: "No internet connection"));
       }
     } catch (e) {
       return left(Failure(errorMessage: e.toString()));
     }
   }
 
-
   @override
-  Future<Either<Failure, MovieSuggetionResponseEntity>> getMovieSuggetion(int id) {
-    // TODO: implement getMovieSuggetion
+  Future<Either<Failure, MovieSuggetionResponseDm>> getMovieSuggestion(int id) async {
+    // TODO: Implement getMovieSuggestion
     throw UnimplementedError();
   }
-
-
-
 }
