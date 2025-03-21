0b6721b09992edb74fb12f3c18bbc9440dc8ca0e
Ahmed <elzek989@gmail.com>
2025-03-21 06:47:46 +0200

    Add BottomNavBar In Home Screen And Add Pages

---
+++ lib/features/home_screen/home_widgets.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:movie_app_route_graduation_project/core/resources/app_colors.dart';
+import 'package:movie_app_route_graduation_project/features/home_screen/pages/explore_page/explore_page.dart';
+import 'package:movie_app_route_graduation_project/features/home_screen/pages/home_page/home_page.dart';
+import 'package:movie_app_route_graduation_project/features/home_screen/pages/profile_page/profile_page.dart';
+import 'package:movie_app_route_graduation_project/features/home_screen/pages/search_page/search_page.dart';
+
+List<BottomNavigationBarItem> generateBottomNavItems(
+    Map<String, String> iconMap) {
+  return iconMap.entries
+      .map((entry) => BottomNavigationBarItem(
+    icon: SvgPicture.asset(entry.key),
+    activeIcon: SvgPicture.asset(entry.key, color: AppColors.orangeColor),
+    backgroundColor: AppColors.darkGrayColor,
+    label: entry.value,))
+      .toList();
+}
+List<Widget> taps = [
+  const HomePage(),
+  const SearchPage(),
+  const ExplorePage(),
+  const ProfilePage(),
+];
